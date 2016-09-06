5f3e565b44f2e90244914592717aa4546a9f4bb0
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-09-06 13:11:05 +0200

    Clear Docker entrypoint

--- djoe-dependencies/docker-entrypoint.sh
+++ djoe-dependencies/docker-entrypoint.sh
@@ -1,15 +1,60 @@
 #!/bin/bash
 
+echo
+echo
+echo Launch can take long time, please be patient ....
+echo Launch can take long time, please be patient ....
+echo Launch can take long time, please be patient ....
+echo Launch can take long time, please be patient ....
+echo Launch can take long time, please be patient ....
+echo Launch can take long time, please be patient ....
+echo Launch can take long time, please be patient ....
+echo
+echo
+
+echo
+echo Used configuration:
+echo
+
+cat /opt/djoe-docker-config.sh
+
+echo
+echo TURN credentials:
+echo
+
+cat /opt/djoe-dependencies/turnCredentials.json
+
+
 export PATH=$PATH:/opt/nodejs4/bin/
 
-# Startup Openfire
+echo
+echo Starting PostgreSQL
+echo
+
+sudo service postgresql start
+
+echo
+echo Starting Openfire
+echo
+
 /opt/openfire/bin/openfire start
 
-# Startup Etherpad in background
+echo
+echo Starting Etherpad
+echo
+
 /opt/etherpad-lite/bin/run.sh --root &
 
-# Startup stats module in background
-node /opt/web-stats/server/server.js &
+echo
+echo Starting Stats
+echo
+
+cd /opt/web-stats/server/
+node server.js &
+
+echo
+echo Starting Apache2
+echo
 
 # Startup Apache2 in foreground
 apachectl -D FOREGROUND
