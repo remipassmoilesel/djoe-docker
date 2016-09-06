b84456a6a950932c11b97508b01b5c188c61ea06
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-09-06 13:10:42 +0200

    Change postgres user password

--- djoe-dependencies/setup-postgres.sh
+++ djoe-dependencies/setup-postgres.sh
@@ -10,8 +10,12 @@ if [ $DJOE_DEBUG_MODE = true ] ; then
   set -x
 fi
 
+# Start servers in order to crate databases
 sudo service postgresql start
 
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres'; ";
+
+# Create needed databases
 sudo -u postgres psql -c 'create database "openfire"'
 sudo -u postgres psql -c 'create database "Stats"'
 
