907b1b44ac58eb65d7406e30e50ddf6d3936fe3a
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-09-06 13:09:00 +0200

    Clearing configuration file

--- djoe-docker-config.sh
+++ djoe-docker-config.sh
@@ -5,14 +5,15 @@
 #
 # /!\ All parameters are COMPULSORY
 #
+# No TURN servers are provided so if you want try videoconference in 'real world' you have to
+# modify djoe-dependencies/turnCredentials.json and add your credentials
+#
 
-# If set to true, verbosity will be increased
-#DJOE_DEBUG_MODE=false
-DJOE_DEBUG_MODE=true
+# XMPP domain, a name representing an XMPP ~~"network"
+DJOE_XMPP_DOMAIN="im.silverpeas.net"
 
-# If set to true, dependencies will be downloaded
-#DJOE_DOWNLOAD_DEPENDENCIES=true
-DJOE_DOWNLOAD_DEPENDENCIES=false
+# HTTP domain, where whole installation can be reached
+DJOE_HTTP_DOMAIN="172.17.0.2"
 
 # If set to true, non certified TLS keys will be created, and behavior of demo website will
 # be adapted. Otherwise you will have to specify where are keys
@@ -21,7 +22,18 @@ DJOE_USE_NON_CERTIF_KEYS=true
 
 # Path of TLS keys, relative to the main Dockerfile directory ($DJOE_GIT_ROOT/opt/docker-djoe/)
 # If option DJOE_USE_NON_CERTIF_KEYS is enabled, these paths will not be used
+
 # NOT EFFECTIVE FOR NOW
+# NOT EFFECTIVE FOR NOW
+# NOT EFFECTIVE FOR NOW
+
 DJOE_SSL_KEY="djoe.key"
 DJOE_SSL_CERT="djoe.crt"
 
+# If set to true, verbosity will be increased
+#DJOE_DEBUG_MODE=false
+DJOE_DEBUG_MODE=true
+
+# If set to true, dependencies will be downloaded
+#DJOE_DOWNLOAD_DEPENDENCIES=true
+DJOE_DOWNLOAD_DEPENDENCIES=false
