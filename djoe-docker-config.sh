48e7c9ec9bd4de6453b93a92453e0416d86203f6
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-09-17 12:34:30 +0200

    Remove uneeded configuration options

--- djoe-docker-config.sh
+++ djoe-docker-config.sh
@@ -15,17 +15,6 @@ DJOE_XMPP_DOMAIN="heydjoe.xmpp"
 # HTTP domain, where whole installation can be reached
 DJOE_HTTP_DOMAIN="www.heydjoe.domain"
 
-# If set to true, non certified TLS keys will be created, and behavior of demo website will
-# be adapted. Otherwise you will have to specify where are keys
-#DJOE_USE_NON_CERTIF_KEYS=false
-DJOE_USE_NON_CERTIF_KEYS=true
-
-# Path of TLS keys, relative to the main Dockerfile directory ($DJOE_GIT_ROOT/opt/docker-djoe/)
-# If option DJOE_USE_NON_CERTIF_KEYS is enabled, these paths will not be used
-# Keys have to be placed in 'djoe-dependencies' folder
-DJOE_SSL_KEY="djoe.key"
-DJOE_SSL_CERT="djoe.crt"
-
 # If set to true, verbosity will be increased
 #DJOE_DEBUG_MODE=false
 DJOE_DEBUG_MODE=true
